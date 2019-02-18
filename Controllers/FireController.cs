3b914e7952345b1f136ad708224355e2615a786a
tob <tob@ecommerce.co.th>
2019-02-18 11:51:33 +0700

    controllerFire

--- Controllers/FireController.cs
+++ Controllers/FireController.cs
@@ -14,14 +14,29 @@ namespace safety_b2c.Controllers
             return View();
         }
 
-        public ActionResult Product()
+        // > Tenement
+        public ActionResult Product_Tenement()
         {
-            return View("~/Views/Fire/Product.aspx");
+            return View("~/Views/Fire/Tenement/Product.aspx");
+        }
+        public ActionResult Buy_Tenement()
+        {
+            return View("~/Views/Fire/Tenement/Buy.aspx");
         }
 
-        public ActionResult Buy()
+        // > Housing
+        public ActionResult Product_Housing()
         {
-            return View("~/Views/Fire/Buy.aspx");
+            return View("~/Views/Fire/Housing/Product.aspx");
         }
+        public ActionResult CalculateInsured_Housing()
+        {
+            return View("~/Views/Fire/Housing/CalculateInsured.aspx");
+        }
+        public ActionResult Buy_Housing()
+        {
+            return View("~/Views/Fire/Housing/Buy.aspx");
+        }
+
     }
 }
